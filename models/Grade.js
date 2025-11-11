c3ab00feab15423639c3e02fded86e02299bfafa
Lương Đoàn Đức Minh <115520105+ducminh0302@users.noreply.github.com>
2025-11-12 01:27:28 +0700

    Delete models/Grade.js

--- models/Grade.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const gradeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  level: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Grade', gradeSchema);

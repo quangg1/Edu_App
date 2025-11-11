338464bc3eea342a708358852944d06c939f7649
Lương Đoàn Đức Minh <115520105+ducminh0302@users.noreply.github.com>
2025-11-12 01:27:58 +0700

    Delete models/Subject.js

--- models/Subject.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const subjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, unique: true },
-  level: { type: String, enum: ["THCS", "THPT"], required: true },
-});
-
-module.exports = mongoose.model("Subject", subjectSchema);
